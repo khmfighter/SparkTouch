3e038c8a06528c4e6b963333e4aad25f18d81444
jasonTangxd <jason.tangxd@gmail.com>
2016-11-17 17:01:54 +0800

    spark sql jdbc

--- spark_scala/src/main/scala/com/xxo/spark/sql/SparkSQLJDBC.scala
+++ spark_scala/src/main/scala/com/xxo/spark/sql/SparkSQLJDBC.scala
@@ -1,24 +1,35 @@
 package com.xxo.spark.sql
 
-import java.sql.DriverManager
+import java.sql.{DriverManager, PreparedStatement}
 
 /**
- *
- * Spark SQL
- * 读取hive
- * Created by xiaoxiaomo on 2016/6/18.
- */
+  * Spark SQL 读取hive
+  *
+  * 运行：
+  * /opt/cloudera/parcels/CDH/bin/spark-submit --class com.xxo.spark.sql.SparkSQLJDBC spark_scala-1.0-SNAPSHOT.jar
+  *
+  * Created by xiaoxiaomo on 2016/11/17.
+  */
 object SparkSQLJDBC {
+
+  //注意用户，不然会有异常
+  // User anonymous does not have privileges for QUERY
   def main(args: Array[String]) {
+
+    //1. 加载驱动
     Class.forName("org.apache.hive.jdbc.HiveDriver")
-    val conn = DriverManager.getConnection("jdbc:hive2://192.168.33.88:10000/default","","")
-    val pstatm = conn.prepareStatement("select * from test")
-    val rs = pstatm.executeQuery()
+    val conn = DriverManager.getConnection("jdbc:hive2://10.141.5.27:10000/default","root","")
+
+    //2. 操作
+    val statement: PreparedStatement = conn.prepareStatement("select * from test_1")
+    val rs = statement.executeQuery()
     while(rs.next()){
       println(rs.getString(1))
     }
+
+    //3. 关闭连接
     rs.close
-    pstatm.close()
+    statement.close()
     conn.close
   }
 }
