7dd2fa69a6416c735850f55713574c43abe5d4f4
jasonTangxd <jason.tangxd@gmail.com>
2016-11-17 13:34:42 +0800

    spark on yarn && standalone

--- spark_scala/src/main/scala/com/xxo/spark/onyarn/OnYarnTest.scala
+++ spark_scala/src/main/scala/com/xxo/spark/onyarn/OnYarnTest.scala
@@ -1,29 +1,44 @@
 package com.xxo.spark.onyarn
 
-import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 
+/**
+  *
+  * 提交任务命令
+  * /opt/cloudera/parcels/CDH/bin/spark-submit --class com.xxo.spark.onyarn.OnYarnTest --master yarn-client /home/txd/spark_scala-1.0-SNAPSHOT.jar /data/test.txt /data/tmp
+  *
+  * 代码开发流程
+  * 1. 构造sc对象  val sc = new SparkContext
+  * 2. 加载数据
+  * 3. 业务逻辑
+  * 4. 输出
+  *
+  * yarn-client 和 yarn-cluster 区别。
+  * 1. yarn-client日志打印比yarn-cluster更加详细。
+  * 2. yarn-client会立即校验程序，而yarn-cluster在程序申请到资源之后执行在校验。
+  * 3. spark Driver的位置不一样，yarn-client和提交client在一台机器，yarn-cluster随机在机器中AM所在机器启动。
+  */
 object OnYarnTest {
   def main(args: Array[String]) {
     
     if(args.length != 2){
-      println("Usage: <inputpath> <outputPath>")
+      println("Usage: <inputPath> <outputPath>")
       System.exit(1)
     }
     
-    //构造spark上下文对象
+    //1. 构造spark上下文对象
     val sc = new SparkContext
     
-    //加载数据
+    //2. 加载数据
     val file = sc.textFile(args(0))
     
-    //计算逻辑
+    //3. 计算逻辑
     val data = file.flatMap(_.split(" ")).map((_,1)).reduceByKey(_ + _)
     
     data.cache
     
-    //保存数据
+    //4. 保存数据
     data.repartition(1).saveAsTextFile(args(1))
     
   }
--- spark_scala/src/main/scala/com/xxo/spark/standalone/StandaloneTest.scala
+++ spark_scala/src/main/scala/com/xxo/spark/standalone/StandaloneTest.scala
@@ -4,35 +4,37 @@ import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 
+
 /**
- * standalone任务提交
+  * standalone 任务提交
+  * /opt/cloudera/parcels/CDH/bin/spark-submit --class com.xxo.spark.standalone.StandaloneTest /home/txd/spark_scala-1.0-SNAPSHOT.jar spark://fetch-loadTest-22:7077 /opt/cloudera/parcels/CDH/lib/spark/ /data/test.txt /data/tmp
  */
 object StandaloneTest {
   def main(args: Array[String]) {
     
     if(args.length != 4){
-      println("Usage: <MasterURL> <SparkHome> <inputpath> <outputPath>")
+      println("Usage: <MasterURL> <SparkHome> <inputPath> <outputPath>")
       System.exit(1)
     }
-	  //构造配置对象
+	  //1. 构造配置对象
     val conf = new SparkConf
-    conf.setAppName("standalone demo") //job名称
+    conf.setAppName("standalone test") //job名称
     conf.setMaster(args(0))  //Spark URL
     conf.setSparkHome(args(1)) //spark安装目录
     conf.set("spark.executor.memory", "2g")  //配置自定义参数
     
-    //构造spark上下文对象
+    //2. 构造spark上下文对象
     val sc = new SparkContext(conf)
     
-    //加载数据
+    //3. 加载数据
     val file = sc.textFile(args(2))
     
-    //计算逻辑
+    //4. 计算逻辑
     val data = file.flatMap(_.split(" ")).map((_,1)).reduceByKey(_ + _)
     
     data.cache
     
-    //保存数据
+    //5. 保存数据
     data.repartition(1).saveAsTextFile(args(3))
     
   }
